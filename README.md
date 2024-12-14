fb86f01a45c74adeb64dbc6a5b546a3c24b9c311
hxrts <sam@hxrts.com>
2024-12-14 14:26:16 -0500

    update nix flake to multi-system and add readme image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # diagram
 model environment for distributed programs
+
+![radar image of a butterfly swarm](./radar.jpg)
--- flake.nix
+++ flake.nix
@@ -1,18 +1,22 @@
 {
-  description = "haskell development environment";
+  description = "Haskell development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs"; 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.defaultApp {
-    inputs = { inherit nixpkgs; };
-    defaultPackage = nixpkgs.lib.mkShell {
-      packages = pkgs: with pkgs.haskellPackages; [
-        ghc              # The Glasgow Haskell Compiler
-        cabal-install    # Tool for building and managing Haskell projects
-        hlint            # Haskell linter
-        haskell-language-server # For IDE integration
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs.haskellPackages; [
+          ghc                    # The Glasgow Haskell Compiler
+          cabal-install          # Tool for building and managing Haskell projects
+          hlint                  # Haskell linter
+          haskell-language-server # For IDE integration
+        ];
+      };
+    });
 }
